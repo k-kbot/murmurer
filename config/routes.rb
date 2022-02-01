9649aaa84cfa618fe72c7374d76dc8c32fa40f13
k-kbot <polykkpodh1243@gmail.com>
2022-02-01 23:35:02 +0900

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'tweets#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Tweets', type: :request do
-  describe 'GET /index' do
-    xit 'returns http success' do
-      get root_path
+RSpec.describe "Sessions", type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get login_path
       expect(response).to have_http_status(:success)
     end
   end
